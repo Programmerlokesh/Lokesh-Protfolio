f97e6567a0a2c6f2418e5725871498a55fd5c892
Lokesh Debnath <lokeshdebnath8@gmail.com>
2022-03-10 22:04:47 +0530

    This is commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import AboutSextion from "./components/About/AboutSextion";
 import Footer from "./components/Footer/Footer";
 import ShootingIndex from "./components/HeroSection/ShootingIndex";
+import Preloader from "./components/Preloader/Preloader";
 
 function App() {
   return (
     <>
+      <Preloader />
       <ShootingIndex />
       <AboutSextion />
       <Footer />
